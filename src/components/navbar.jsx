8e25f31d8bf9a61f212365280a4d78c8815b496a
KhingRozy <sundaymodupe200@gmail.com>
2023-09-06 16:31:22 +0100

    import all the useful images

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import  from "mjml-navbar"
+import {} from "rrr";
+
+export default function Navbar() {
+  return (
+    <section>
+      <div>
+        <nav></nav>
+      </div>
+    </section>
+  );
+}
